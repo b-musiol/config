2e549c73d7fdd2b0aa9261f2b7bfa65ac3af4913
Bartek Musiol <bmusiol@proton.me>
2026-07-12 22:29:11 +0200

    hyprland: kill bind

--- hyprland/dotfiles/hyprland.lua
+++ hyprland/dotfiles/hyprland.lua
@@ -286,6 +286,7 @@ local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
 hl.bind(mainMod .. " + return", hl.dsp.exec_cmd(terminal))
 local closeWindowBind = hl.bind(mainMod .. " + Q", hl.dsp.window.close())
+hl.bind(mainMod .. " + CTRL + SHIFT + ALT + Q", hl.dsp.window.kill())
 -- closeWindowBind:set_enabled(false)
 hl.bind("CTRL + ALT + DELETE", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
