4fa51b048ff984db1172920766152a24ccf288a6
Bartek Musiol <bmusiol@proton.me>
2026-07-25 20:39:22 +0200

    hyprland: adjust rounding to noctalia

--- hyprland/dotfiles/hyprland.lua
+++ hyprland/dotfiles/hyprland.lua
@@ -131,7 +131,7 @@ hl.config({
     },
 
     decoration = {
-        rounding       = 12,
+        rounding       = 16,
         rounding_power = 2,
 
         -- Change transparency of focused and unfocused windows
