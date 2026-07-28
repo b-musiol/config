35a2e07cd3eac6c9cdf8a47790a5b94a5c057bed
Bartek Musiol <bmusiol@proton.me>
2026-07-28 21:03:01 +0200

    hyprland: fish config

---
+++ hyprland/dotfiles/fish/config.fish
@@ -0,0 +1,50 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+function fish_greeting
+	fastfetch
+
+	#set -l llmtext "LLMs are the asbestos of the 21st century"
+	#set -l cols (tput cols)
+	#set -l len (string length --visible -- "$llmtext")
+	#set -l padding ( math "floor(($cols - $len) / 2)")
+
+	#if test $padding -lt 0
+	#	set padding 0
+	#end
+
+	#printf "%*s%s\n" $padding "" "$llmtext"
+
+	echo (set_color 59000e)"                                  LLMs are the asbestos of the 21st century!"(set_color normal)
+end
+function fish_prompt
+    # Exit status handling
+    set -l st $status
+    set -l st_part ""
+    if test $st -ne 0
+        set st_part (set_color red)"[$st]"(set_color normal)' '
+    end
+
+    # Current directory
+    set -l d (pwd)
+
+    # Replace $HOME with ~
+    if string match -q "$HOME*" -- $d
+        set d (string replace -r "^$HOME" "~" -- $d)
+    end
+
+    # Prompt character
+    set -l pchar '$'
+    if test (id -u) -eq 0
+        set pchar '#'
+    end
+
+    # Render prompt
+    echo -n " "
+    echo -n $st_part
+    echo -n (set_color 960018)$d
+    echo -n (set_color 34a089)$pchar' '(set_color normal)
+end
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
